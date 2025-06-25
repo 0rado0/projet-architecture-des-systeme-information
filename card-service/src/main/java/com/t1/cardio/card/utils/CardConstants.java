654932464d4996420617c07d9b4fd8beb30eb8d6
rado <130479283+0rado0@users.noreply.github.com>
2025-06-25 14:11:05 +0200

    Update CardConstants.java

--- card-service/src/main/java/com/t1/cardio/card/utils/CardConstants.java
+++ card-service/src/main/java/com/t1/cardio/card/utils/CardConstants.java
@@ -16,5 +16,5 @@ public class CardConstants {
             (DEBUG ? "/img-service/fake/prompt/req" : "/img-service/prompt/req");
     public static final String IMAGE_POLL_REQUEST_URL = IMAGE_SERVICE_BASE_URL + "/img-service/prompt/req/api";
 
-    public static final String API_TOKEN = "1d4b77b3-7d7b-4ee1-b0ad-ce89a2e2f0be";
+    public static final String API_TOKEN = "your-token-here";
 }
